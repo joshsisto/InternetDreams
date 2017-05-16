22755dbe6023677cd663a1f672b9055b81909bb2
Josh Sisto <joshsisto@gmail.com>
2017-05-15 19:55:04 -0700

    Update deepDream.py

--- deepDream.py
+++ deepDream.py
@@ -18,11 +18,12 @@ except:
 ## Login to https://deepdreamgenerator.com/login
 email_field = browser.find_element_by_name('email')
 password_field = browser.find_element_by_name('password')
-email_field.send_keys('joshsisto@gmail.com')   # Email account used
+email_field.send_keys('josh@joshsisto.com')   # Email account used
 password_field.send_keys('Your Password')     # Password for account
+time.sleep(3)
 password_field.submit()
 
-time.sleep(3)
+time.sleep(15)
 
 if __name__ == '__main__':
 
@@ -37,22 +38,24 @@ if __name__ == '__main__':
         # click Deep Style on the menu. https://deepdreamgenerator.com/generator-style
         linkElem = browser.find_element_by_link_text('Deep Dream').click()
 
-        time.sleep(3)
+        time.sleep(5)
 
         ### settings
         # click the settings drop down menu
         browser.find_element_by_link_text("Settings").click()
 
-        time.sleep(3)
+        time.sleep(5)
 
-        ## dream type - no change for default
-        # spirits
-#        browser.find_element_by_xpath("(//input[@name='type'])[2]").click()
-        # neuron
-#        browser.find_element_by_xpath("(//input[@name='type'])[3]").click()
-        # valyrian
-#        browser.find_element_by_xpath("(//input[@name='type'])[4]").click()
-        # use your own
+        # choose a random dream type
+        random_type = random.choice(range(4))
+        if random_type == int(0):
+            browser.find_element_by_xpath("(//input[@name='type'])[2]").click()
+        elif random_type == int(1):
+            browser.find_element_by_xpath("(//input[@name='type'])[2]").click()
+        elif random_type == int(2):
+            browser.find_element_by_xpath("(//input[@name='type'])[2]").click()
+        else:
+            print('not using any dream type')
 
         ## resolution
         # normal
@@ -63,76 +66,51 @@ if __name__ == '__main__':
 #        browser.find_element_by_xpath("(//input[@name='resolution'])[4]").click()
 
         ## inception depth
-        # normal (no change)
+        # normal - comment out (#deep) for normal
         # deep
         browser.find_element_by_xpath("(//input[@name='depth'])[2]").click()
 
-
-        ## neural network layer
+        ## click the layer area and choose a random layer
         browser.find_element_by_name("layer").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[2]").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[3]").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[4]").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[5]").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[6]").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[7]").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[8]").click()
-        browser.find_element_by_xpath("(//input[@name='layer'])[9]").click()
-        # browser.find_element_by_xpath("(//input[@name='layer'])[10]").click()
+        random_layer = random.choice(range(9))
+        if random_layer == int(0):
+            browser.find_element_by_xpath("(//input[@name='layer'])[2]").click()
+        elif random_layer == int(1):
+            browser.find_element_by_xpath("(//input[@name='layer'])[3]").click()
+        elif random_layer == int(2):
+            browser.find_element_by_xpath("(//input[@name='layer'])[4]").click()
+        elif random_layer == int(3):
+            browser.find_element_by_xpath("(//input[@name='layer'])[5]").click()
+        elif random_layer == int(4):
+            browser.find_element_by_xpath("(//input[@name='layer'])[6]").click()
+        elif random_layer == int(5):
+            browser.find_element_by_xpath("(//input[@name='layer'])[7]").click()
+        elif random_layer == int(6):
+            browser.find_element_by_xpath("(//input[@name='layer'])[8]").click()
+        elif random_layer == int(7):
+            browser.find_element_by_xpath("(//input[@name='layer'])[9]").click()
+        elif random_layer == int(8):
+            browser.find_element_by_xpath("(//input[@name='layer'])[10]").click()
 
-        time.sleep(3)
+        time.sleep(5)
 
         # change from private to public
         browser.find_element_by_xpath("(//input[@name='access'])[2]").click()
 
-        time.sleep(3)
+        time.sleep(5)
 
-        ## Upload dream image from file location and submit
+        ## upload dream image from file location and submit
         elem_xpath = browser.find_element_by_xpath('//input[@type=\'file\']')
         elem_xpath.send_keys('C:\\dream\\dream_image\\%s' % (random_image))
         elem_xpath.submit()
 
-        time.sleep(180)    # wait 3 minutes
+        time.sleep(120)    # wait 2 minutes
 
-        # click 'GO DEEPER'
-        browser.find_element_by_xpath("//a[3]/span").click()
-        time.sleep(5)
-        browser.find_element_by_xpath("//button[@type='submit']").click()
+        ## click go deeper and submit 3 times
+        for x in range(0, 3):
+            browser.find_element_by_xpath("//a[3]/span").click()
+            time.sleep(5)
+            browser.find_element_by_xpath("//button[@type='submit']").click()
+            time.sleep(90)
 
-        time.sleep(180)  # wait 3 minutes
-
-        # click 'GO DEEPER'
-        browser.find_element_by_xpath("//a[3]/span").click()
-        time.sleep(5)
-        browser.find_element_by_xpath("//button[@type='submit']").click()
-
-        time.sleep(180)  # wait 3 minutes
-
-        # click 'GO DEEPER'
-        browser.find_element_by_xpath("//a[3]/span").click()
-        time.sleep(5)
-        browser.find_element_by_xpath("//button[@type='submit']").click()
-
-        time.sleep(180)  # wait 3 minutes
-
-        # click 'GO DEEPER'
-        browser.find_element_by_xpath("//a[3]/span").click()
-        time.sleep(5)
-        browser.find_element_by_xpath("//button[@type='submit']").click()
-
-        time.sleep(180)  # wait 3 minutes
-
-        # click 'GO DEEPER'
-        browser.find_element_by_xpath("//a[3]/span").click()
-        time.sleep(5)
-        browser.find_element_by_xpath("//button[@type='submit']").click()
-
-        time.sleep(180)  # wait 3 minutes
-
-        # click 'GO DEEPER'
-        browser.find_element_by_xpath("//a[3]/span").click()
-        time.sleep(5)
-        browser.find_element_by_xpath("//button[@type='submit']").click()
-
-        # wait for it to process and repeat
-        time.sleep(30)
+        time.sleep(30)   # wait for it to process and repeat
