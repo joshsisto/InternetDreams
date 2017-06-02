c7988ce15db19eb97f2396152712d5da8158c923
Josh Sisto <joshsisto@gmail.com>
2017-06-01 18:58:50 -0700

    Update dreamScraper.py

--- dreamScraper.py
+++ dreamScraper.py
@@ -1,35 +1,18 @@
-
+#! python3
 import requests
-from lxml import html
+
 header = {'Accept-Encoding': 'identity, deflate, compress, gzip', 'Accept': '*/*', 'User-Agent': 'python-requests/1.2.0'}
 login_url = 'https://deepdreamgenerator.com/login'
 gallery_url = 'https://deepdreamgenerator.com/gallery/user/370099'
 
 session_requests = requests.session()
-result = session_requests.get(login_url)
-tree=html.fromstring(result.text)
-auth_token = list(set(tree.xpath('//*[@name="login[_token]"]/@value')))
-auth_iovat = list(set(tree.xpath('//*[@name="login[iovation]"]/@value')))
-# create payload
-payload = {
-    "email": "josh@joshsisto.com",
-    "password": "<PASSWORD>",
-    "login[_token]": auth_token,
-        "login[iovation]": auth_iovat,
-        "login[redir]": "/gallery/user/370099"
-}
 
-#perform login
-result=session_requests.post(login_url, data = payload, headers = dict(referer = login_url))
+## payload = dictionary containing your email and password for https://deepdreamgenerator.com/login
+payload = dict(email="joshsisto@gmail.com", password="forjosh1")
 
-#test the result
-#print(result.text)
+result = session_requests.post(login_url, data = payload, headers = dict(referer = login_url))
 
-
-session_requests2 = requests.session()
 result2 = session_requests.get(gallery_url)
-tree=html.fromstring(result2.text)
 
-result2=session_requests.get(gallery_url)
-
+print(result.text)
 print(result2.text)
